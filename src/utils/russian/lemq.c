6d02039c57e3a31381aa3604b6779c23bcde147a
Sergey Bochenkov <bachan@j3qq4.org>
2011-07-27 21:55:12 +0400

    libturglem-0.2.30

--- src/utils/russian/lemq.c
+++ src/utils/russian/lemq.c
@@ -20,6 +20,7 @@
 
 #define TRUS_SHOW_NFORM    (1 << 0)
 #define TRUS_SHOW_PARADIGM (1 << 1)
+#define TRUS_SHOW_GRAMMEM  (1 << 2)
 
 typedef struct trus_getopt
     trus_getopt_t;
@@ -34,6 +35,7 @@ static struct option options [] =
 {
     { "nform"         , 0, NULL, 'n' },
     { "paradigm"      , 0, NULL, 'p' },
+    { "grammem"       , 0, NULL, 'g' },
     { "no-prediction" , 0, NULL, 'P' },
     {  NULL           , 0, NULL,  0  },
 };
@@ -45,15 +47,17 @@ int trus_getopt_usage(int argc, char **argv)
         "Usage:                                          \n"
         "  %s -[P]n [WORD]...                            \n"
         "  %s -[P]p [WORD]...                            \n"
+        "  %s -g [GRAMMEM]...                            \n"
         "                                                \n"
         "Options:                                        \n"
         "  -n, --nform         : show normal form only   \n"
         "  -p, --paradigm      : show the whole paradigm \n"
+        "  -g, --grammem       : show words with grammem \n"
         "                                                \n"
         "  -P, --no-prediction : don't use prediction    \n"
         "                                                \n"
 
-    , argv[0], argv[0]);
+    , argv[0], argv[0], argv[0]);
 
     return 0;
 }
@@ -62,12 +66,13 @@ int trus_getopt_parse(int argc, char **argv, trus_getopt_t *opts)
 {
     int c;
 
-    while (-1 != (c = getopt_long(argc, argv, "npP", options, NULL)))
+    while (-1 != (c = getopt_long(argc, argv, "npgP", options, NULL)))
     {
         switch (c)
         {
             case 'n': opts->show |= TRUS_SHOW_NFORM;    break;
             case 'p': opts->show |= TRUS_SHOW_PARADIGM; break;
+            case 'g': opts->show |= TRUS_SHOW_GRAMMEM;  break;
 
             case 'P': opts->no_prediction = 1; break;
 
@@ -122,6 +127,8 @@ static int lemmatize_and_print(turglem lem, const char *s, size_t sz_s, trus_get
         {
             fsz = turglem_paradigms_get_form_count(lem->paradigms, form[2*i]);
 
+            /* printf("\033[1mPARADIGM  FORM                                                       GRAMMEM  POS\033[m\n"); */
+
             for (j = 0; j < fsz; ++j)
             {
                 MAFSA_letter letter_dst [1024];
@@ -166,10 +173,54 @@ static int lemmatize_and_print(turglem lem, const char *s, size_t sz_s, trus_get
     return 0;
 }
 
+turglem lem;
+
+void check_grammem_and_print(void *user_data, const MAFSA_letter *l, size_t sz_l)
+{
+    int form [2];
+
+    MAFSA_automaton_str_to_int_pair(l, sz_l, RUSSIAN_LETTER_DELIM, form);
+
+    uint64_t grm = turglem_paradigms_get_grammem(lem->paradigms, form[0], form[1]);
+
+    uint64_t *grm_mask = (uint64_t *) user_data;
+
+    if ((grm & *grm_mask) == *grm_mask)
+    {
+        char binary_dst [1024];
+        size_t sz_binary_dst;
+
+        sz_binary_dst = RUSSIAN_conv_letter_to_binary_utf8(l, sz_l, binary_dst, 1024);
+
+        char *binary_dst_end = memchr(binary_dst, '|', sz_binary_dst);
+        if (NULL != binary_dst_end) sz_binary_dst = binary_dst_end - binary_dst;
+
+        uint8_t pos;
+
+        pos = turglem_paradigms_get_part_of_speech(lem->paradigms, form[0], form[1]);
+
+        size_t k;
+        size_t grm_pos;
+          char grm_str [256];
+
+        grm_pos = snprintf(grm_str, 256, ",");
+
+        for (k = 0; k < 64; ++k)
+        {
+            if (grm & (1ULL << k))
+            {
+                grm_pos += snprintf(grm_str + grm_pos, 256 - grm_pos,
+                    "%"PRIuMAX",", (uintmax_t) k);
+            }
+        }
+
+        printf("%8d  %4"PRIuMAX"  %s  %3"PRIu8"  %.*s\n", form[0], (uintmax_t) form[1],
+            grm_str, pos, (int) sz_binary_dst, binary_dst);
+    }
+}
+
 int main(int argc, char **argv)
 {
-    turglem lem;
-
     int err_no;
     int err_what;
     int i;
@@ -199,6 +250,23 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+    if (opts.show & TRUS_SHOW_GRAMMEM)
+    {
+        uint64_t grm_mask = 0ULL;
+
+        for (i = optind; i < argc; ++i)
+        {
+            grm_mask |= (1ULL << atoi(argv[i]));
+        }
+
+        MAFSA_letter tmp [1024];
+
+        MAFSA_automaton_enumerate(lem->words, NULL, 0, tmp, 1024,
+            &grm_mask, check_grammem_and_print);
+
+        goto end;
+    }
+
     for (i = optind; i < argc; ++i)
     {
         lemmatize_and_print(lem, argv[i], strlen(argv[i]), &opts);
@@ -225,6 +293,7 @@ int main(int argc, char **argv)
         lemmatize_and_print(lem, word, sz, &opts);
     }
 
+end:
     turglem_close(lem);
 
     return 0;
